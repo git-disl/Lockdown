4013b3c9cf931d086031cbe3e6bdc929e5723bd4
LockdownAuthor <noemail>
2023-01-26 10:37:17 +0800

    commit

--- src/script/cifar100.sh
+++ src/script/cifar100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd  ../                            # Change to working directory
-nohup python federated.py   --method fedavg --aggr avg   --data cifar100
-nohup python federated.py   --method lockdown --aggr mask_avg   --data cifar100
-nohup python federated.py   --method krum --aggr avg   --data cifar100
-nohup python federated.py   --method rlr --aggr avg  --theta 8 --data cifar100
+nohup python federated.py   --method fedavg --aggr avg   --data cifar100 &
+nohup python federated.py   --method lockdown --aggr mask_avg   --data cifar100 &
+nohup python federated.py   --method krum --aggr avg   --data cifar100 &
+nohup python federated.py   --method rlr --aggr avg  --theta 8 --data cifar100 &
 wait
